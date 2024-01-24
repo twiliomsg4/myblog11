34244327fe141c657a89adc94bd3b870255a5f54
arfa <arfakaunain51846@gmail.com>
2024-01-24 18:04:17 +0530

    127:Create Save Post Feature Entity class added

---
+++ src/main/java/com/myblog/myblog11/entity/Post.java
@@ -0,0 +1,28 @@
+package com.myblog.myblog11.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String content;
+}
+
+
+
